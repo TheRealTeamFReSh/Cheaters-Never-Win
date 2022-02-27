9b91d433481cda79cfe07c4d6fb7102bf90e696a
Alexandre F <nightlyside@gmail.com>
2022-02-27 14:29:32 +0100

    Adding foreground to the console

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
+use bevy::log::*;
 use bevy::prelude::*;
 
+mod console;
+
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(console::ConsolePlugin)
+        .add_startup_system(setup)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    trace!("Setting up cameras");
+    commands.spawn_bundle(UiCameraBundle::default());
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
 }

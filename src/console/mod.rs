f1b4fffcf971f37c07cbe5769269eb0cdf748d0d
Alexandre F <nightlyside@gmail.com>
2022-02-27 17:15:17 +0100

    Better input handeling and hiding overflow

--- src/console/mod.rs
+++ src/console/mod.rs
@@ -1,163 +1,75 @@
-use bevy::log::*;
 use bevy::prelude::*;
+
+mod input;
+mod ui;
 
 pub struct ConsolePlugin;
 
 impl Plugin for ConsolePlugin {
     fn build(&self, app: &mut App) {
         app.insert_resource(ConsoleData {
-            _command: String::from("my command"),
+            input: String::from("my command"),
             lines: vec![String::from("hello"), String::from("world")],
         })
-        .add_startup_system(setup)
-        .add_system(hide_foreground)
+        .add_startup_system(ui::build_ui)
+        .add_system(ui::hide_foreground)
+        .add_system(input::handle_input_keys)
+        .add_system(update_input_area)
         .add_system(update_lines_area);
     }
 }
 
 pub struct ConsoleData {
-    _command: String,
+    input: String,
     lines: Vec<String>,
-}
-
-// Debug function to hide the foreground
-fn hide_foreground(
-    keyboard: Res<Input<KeyCode>>,
-    mut query: Query<&mut Visibility, With<ConsoleForeground>>,
-) {
-    if keyboard.just_pressed(KeyCode::H) {
-        if let Ok(mut visibility) = query.get_single_mut() {
-            trace!("[ConsolePlugin] toggling foreground visibility");
-            visibility.is_visible = !visibility.is_visible;
-        } else {
-            error!("[ConsolePlugin] Foreground not found!");
-        }
-    }
-}
-
-#[derive(Component)]
-pub struct ConsoleForeground;
-
-#[derive(Component)]
-pub struct LinesArea;
-
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>, window: Res<Windows>) {
-    info!("[ConsolePlugin] Enabling");
-
-    let current_window = window.get_primary().unwrap();
-
-    // ---------- UI COMPONENTS ----------//
-
-    // root component
-    let parent_component = NodeBundle {
-        style: Style {
-            size: Size::new(
-                Val::Px(current_window.width()),
-                Val::Px(current_window.height()),
-            ),
-            justify_content: JustifyContent::Center,
-            align_items: AlignItems::Center,
-            ..Default::default()
-        },
-        color: Color::rgba_u8(0, 0, 0, 0).into(),
-        ..Default::default()
-    };
-
-    // crt overlay
-    let foreground_component = ImageBundle {
-        style: Style {
-            position_type: PositionType::Absolute,
-            size: Size::new(
-                Val::Px(current_window.width()),
-                Val::Px(current_window.height()),
-            ),
-            ..Default::default()
-        },
-        image: asset_server.load("crt.png").into(),
-        ..Default::default()
-    };
-
-    // console root
-    let console_component = NodeBundle {
-        style: Style {
-            size: Size::new(
-                Val::Px(current_window.width() / 2.0),
-                Val::Px(current_window.height() / 1.5),
-            ),
-            padding: Rect {
-                left: Val::Percent(2.0),
-                right: Val::Percent(2.0),
-                top: Val::Percent(2.0),
-                bottom: Val::Percent(2.0),
-            },
-            ..Default::default()
-        },
-        color: Color::rgb_u8(5, 17, 0).into(),
-        ..Default::default()
-    };
-
-    // lines area
-    let lines_area_component = NodeBundle {
-        style: Style {
-            size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
-            ..Default::default()
-        },
-        color: Color::rgba_u8(0, 0, 0, 0).into(),
-        ..Default::default()
-    };
-
-    // ---------- UI TREE CONSTRUCTION ----------//
-    commands
-        .spawn_bundle(parent_component)
-        .with_children(|parent| {
-            // console
-            parent
-                .spawn_bundle(console_component)
-                .with_children(|parent| {
-                    // console lines
-                    parent
-                        .spawn_bundle(lines_area_component)
-                        .with_children(|parent| {
-                            // placeholder to be populated with lines
-                            parent
-                                .spawn_bundle(TextBundle {
-                                    ..Default::default()
-                                })
-                                .insert(LinesArea);
-                        });
-                });
-            // foreground
-            parent
-                .spawn_bundle(foreground_component)
-                .insert(ConsoleForeground);
-        });
-
-    info!("[ConsolePlugin] Enabled");
 }
 
 pub fn update_lines_area(
     data: Res<ConsoleData>,
     asset_server: Res<AssetServer>,
-    mut logs_area_query: Query<&mut Text, With<LinesArea>>,
+    mut lines_area_query: Query<&mut Text, With<ui::LinesArea>>,
 ) {
-    let sections = data
-        .lines
-        .iter()
-        .flat_map(|msg| {
-            let mut msg = msg.clone();
-            msg.push('\n');
+    let sections_text = data.lines.join("\n");
+    let sections = vec![TextSection {
+        value: sections_text,
+        style: TextStyle {
+            font: asset_server.load("fonts/VT323-Regular.ttf"),
+            font_size: 16.,
+            color: Color::rgba_u8(76, 207, 76, 255),
+        },
+    }];
 
-            IntoIterator::into_iter([TextSection {
-                value: msg.clone(),
-                style: TextStyle {
-                    font: asset_server.load("fonts/VT323-Regular.ttf"),
-                    font_size: 16.,
-                    color: Color::rgba_u8(76, 207, 76, 255),
-                },
-            }])
-        })
-        .collect::<Vec<_>>();
-
-    let mut text = logs_area_query.single_mut();
+    let mut text = lines_area_query.single_mut();
     text.sections = sections;
+}
+
+pub fn update_input_area(
+    mut command_input_query: Query<&mut Text, With<ui::CommandInput>>,
+    mut state: ResMut<ConsoleData>,
+    asset_server: Res<AssetServer>,
+    time: Res<Time>,
+) {
+    let mut text = command_input_query.single_mut();
+    text.sections = vec![];
+
+    if state.input.len() > 144 {
+        let trimmed_command = state.input[..144].to_string();
+        state.input = trimmed_command;
+    }
+
+    let mut to_show = String::from("user@hacked_pc > ");
+    to_show.push_str(&state.input);
+
+    if (time.seconds_since_startup() * 3.0) as u64 % 2 == 0 {
+        to_show.push('_');
+    }
+
+    text.sections.push(TextSection {
+        value: to_show,
+        style: TextStyle {
+            font: asset_server.load("fonts/VT323-Regular.ttf"),
+            font_size: 16.,
+            color: Color::rgba_u8(102, 255, 102, 255),
+        },
+    });
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
-use bevy::log::*;
 use bevy::prelude::*;
+use bevy_inspector_egui::WorldInspectorPlugin;
 
 mod console;
 
 fn main() {
     App::new()
+        .insert_resource(WindowDescriptor {
+            resizable: false,
+            height: 720.,
+            width: 1280.,
+            title: "Bevy Jam #1".to_string(),
+            ..Default::default()
+        })
         .add_plugins(DefaultPlugins)
+        .add_plugin(WorldInspectorPlugin::new())
         .add_plugin(console::ConsolePlugin)
         .add_startup_system(setup)
         .run();

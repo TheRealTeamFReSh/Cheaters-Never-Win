e55f663c268843ac622b0ae2df67a63235d4a7b7
Micah Tigley <tigleym@gmail.com>
2022-02-27 13:53:04 -0500

    Start initial implementation of basic runner

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
 use bevy::prelude::*;
 
+mod runner;
+
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    let window = WindowDescriptor {
+        title: "Bevy Jam".to_string(),
+        width: 1280.0,
+        height: 720.0,
+        vsync: true,
+        resizable: false,
+        ..Default::default()
+    };
+
+    App::new()
+        .insert_resource(window)
+        .add_plugins(DefaultPlugins)
+        .add_plugin(runner::RunnerPlugin)
+        .run();
 }

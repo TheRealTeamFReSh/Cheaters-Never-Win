33d41489a5116a13a7837496c474f93b51b8ab85
Carlo Supina <cdsupina@gmail.com>
2022-03-02 21:43:26 -0600

    add single jump

--- src/runner/player.rs
+++ src/runner/player.rs
@@ -11,10 +11,19 @@ pub struct Player {
     pub speed: f32,
     pub acceleration: f32,
     pub deceleration: f32,
+    pub feet_touching_platforms: FeetTouchingPlatforms,
+}
+
+#[derive(Debug)]
+pub struct FeetTouchingPlatforms {
+    pub platforms: Vec<Entity>,
 }
 
 #[derive(Component)]
 pub struct PlayerAnimationTimer(Timer);
+
+#[derive(Component)]
+pub struct PlayerFeet;
 
 pub struct PlayerPlugin;
 
@@ -30,7 +39,8 @@ impl Plugin for PlayerPlugin {
                     .with_system(follow_player_camera)
                     .with_system(animate_sprite)
                     .with_system(move_character)
-                    .with_system(detect_char_interactable),
+                    .with_system(detect_char_interactable)
+                    .with_system(player_feet),
             );
     }
 }
@@ -49,6 +59,7 @@ fn spawn_character(
         speed: 8.0,
         acceleration: 0.12,
         deceleration: 0.1,
+        feet_touching_platforms: FeetTouchingPlatforms { platforms: vec![] },
     };
 
     let collider_size_hx = 30.0 / rapier_config.scale / 2.0;
@@ -86,7 +97,69 @@ fn spawn_character(
                     ..Default::default()
                 })
                 .insert(PlayerAnimationTimer(Timer::from_seconds(0.1, true)));
+
+            parent
+                .spawn_bundle(ColliderBundle {
+                    shape: ColliderShape::cuboid(
+                        collider_size_hx,
+                        35.0 / rapier_config.scale / 2.0,
+                    )
+                    .into(),
+                    position: [0.0, -35.0 / rapier_config.scale / 2.0].into(),
+                    collider_type: ColliderType::Sensor.into(),
+                    flags: ColliderFlags {
+                        active_events: ActiveEvents::INTERSECTION_EVENTS,
+                        ..Default::default()
+                    }
+                    .into(),
+                    ..Default::default()
+                })
+                .insert(PlayerFeet);
         });
+}
+
+pub fn player_feet(
+    mut intersection_events: EventReader<IntersectionEvent>,
+    player_feet_query: Query<Entity, With<PlayerFeet>>,
+    mut player_query: Query<&mut Player>,
+) {
+    for event in intersection_events.iter() {
+        let collider1_entity = event.collider1.entity();
+        let collider2_entity = event.collider2.entity();
+        for feet_entity in player_feet_query.iter() {
+            for mut player in player_query.iter_mut() {
+                if event.intersecting {
+                    if collider1_entity == feet_entity {
+                        player
+                            .feet_touching_platforms
+                            .platforms
+                            .push(collider2_entity);
+                    } else if collider2_entity == feet_entity {
+                        player
+                            .feet_touching_platforms
+                            .platforms
+                            .push(collider1_entity);
+                    }
+                } else if collider1_entity == feet_entity {
+                    let index = player
+                        .feet_touching_platforms
+                        .platforms
+                        .iter()
+                        .position(|x| *x == collider2_entity)
+                        .unwrap();
+                    player.feet_touching_platforms.platforms.remove(index);
+                } else if collider2_entity == feet_entity {
+                    let index = player
+                        .feet_touching_platforms
+                        .platforms
+                        .iter()
+                        .position(|x| *x == collider1_entity)
+                        .unwrap();
+                    player.feet_touching_platforms.platforms.remove(index);
+                }
+            }
+        }
+    }
 }
 
 pub fn animate_sprite(
@@ -123,8 +196,10 @@ fn move_character(
         let left = keyboard_input.pressed(KeyCode::A);
         let right = keyboard_input.pressed(KeyCode::D);
 
+        // TODO: check if player is on the ground
         let jump = cheat_codes.is_code_activated(&CheatCodeKind::Jump)
-            && keyboard_input.just_released(KeyCode::Space);
+            && keyboard_input.just_released(KeyCode::Space)
+            && !player.feet_touching_platforms.platforms.is_empty();
 
         let x_axis = -(left as i8) + right as i8;
 
@@ -144,7 +219,7 @@ fn move_character(
         }
 
         if jump {
-            physics::jump(1200.0, &mut rb_vel, rb_mprops)
+            physics::jump(1500.0, &mut rb_vel, rb_mprops)
         }
     }
 }

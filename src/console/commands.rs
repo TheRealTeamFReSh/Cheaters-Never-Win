7d1af144413a4d92d6a0a32ee227d29c9786ca37
Micah Tigley <tigleym@gmail.com>
2022-03-04 11:13:58 -0500

    Prevent activating codes that are valid but the player does not have the letters collected for it.

--- src/console/commands.rs
+++ src/console/commands.rs
@@ -91,6 +91,15 @@ pub fn is_valid_cheat(
             let char_entry = collected_chars.values_map.get(&ch);
             if let Some(_count) = char_entry {
                 *collected_chars.values_map.get_mut(&ch).unwrap() -= 1;
+                // Update values map
+                let char_entry = collected_chars.values_map.get(&ch);
+                if let Some(_count) = char_entry {
+                    *collected_chars.values_map.get_mut(&ch).unwrap() -= 1;
+                }
+            } else {
+                collected_chars.values = original_collected_chars;
+                collected_chars.values_map = original_collected_chars_map;
+                return false;
             }
         }
         return true;
